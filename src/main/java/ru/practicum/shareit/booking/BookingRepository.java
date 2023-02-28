515faa8a1ca2367a973ae7cbb7ec9912a90704ff
Fedor Malcev <mallcevfs@yandex.ru>
2023-02-28 13:23:27 +0300

    Закночил с новыми проверками для постман

--- src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.booking;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.user.model.User;
 
@@ -15,8 +16,10 @@ public interface BookingRepository extends JpaRepository<Booking, Integer> {
 
     List<Booking> findAllByItemIdOrderByStartDesc(int itemId);
 
+    @Query("SELECT b FROM Booking b WHERE b.item.id = :itemId AND b.end < :end AND b.status != 'REJECTED' ORDER BY b.start DESC, b.status")
     List<Booking> findAllByItemIdAndEndBeforeOrderByStartDesc(int itemId, LocalDateTime end);
 
+    @Query("SELECT b FROM Booking b WHERE b.item.id = :itemId AND b.start > :start AND b.status != 'REJECTED' ORDER BY b.start ASC, b.status")
     List<Booking> findAllByItemIdAndStartAfterOrderByStartAsc(int itemId, LocalDateTime start);
 
     Optional<Booking> findFirstByBookerAndItemIdAndEndBefore(User booker, int itemId, LocalDateTime date);
